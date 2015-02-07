eeeb9374ab43775605dfea4f852d2f86e2c1102f
Agilio Padua <agilio.padua@gmail.com>
2015-02-06 19:45:48 -0500

    Added formula for xcrysden.

---
+++ xcrysden.rb
@@ -0,0 +1,37 @@
+class Xcrysden < Formula
+  homepage "http://www.xcrysden.org/"
+  url "http://www.xcrysden.org/download/xcrysden-1.5.60.tar.gz"
+  version "1.5.60"
+  sha1 "388cf837aa757e612f36a12eb7fb592c36cfd6f3"
+
+  depends_on :x11
+  depends_on :fortran
+  depends_on "homebrew/dupes/tcl-tk" => "with-x11"
+  depends_on "fftw" => "with-fortran"
+  depends_on "wget"
+  
+  # Fix togl -accum false in Tcl and modify Make.sys
+  patch do
+    url "https://gist.github.com/1f9fd5e8a598018f03b6.git"
+    sha1 "21ab6baf87cfeecb79405154c23a5f6d214f7501"
+  end
+
+  def install
+    system "cp", "system/Make.macosx-x11", "Make.sys"
+
+    ENV.deparallelize
+    system "make", "xcrysden"
+    system "make", "install"
+  end
+
+  def caveats
+    <<-EOS.undent
+      XCrySDen can be user-customized. Create $HOME/.xcrysden/ directory
+      and copy the "custom-definitions" and "Xcrysden_resources" files
+      from the Tcl/ subdirectory of the XCrySDen root directory.
+      These can be then modified according to user preference.
+
+      For more info about customization, see: http://www.xcrysden.org/doc/custom.html
+    EOS
+  end
+end
