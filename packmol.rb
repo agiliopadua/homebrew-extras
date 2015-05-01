6696c783e6d1bcb1194f726d832107df69be6451
Agilio Padua <agilio.padua@gmail.com>
2015-05-01 18:47:52 -0400

    Version update

--- packmol.rb
+++ packmol.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Packmol < Formula
   homepage "http://www.ime.unicamp.br/~martinez/packmol/"
-  url "http://leandro.iqm.unicamp.br/packmol/versionhistory/packmol-14.225.tar.gz"
-  sha1 "024c2ef3df4adc584301b6124646f80b200984c8"
+  url "http://leandro.iqm.unicamp.br/packmol/versionhistory/packmol-15.084.tar.gz"
+  sha1 "1820362b56c11e59ac95e6bc2440ae3cf6c27ff1"
 
   env :std
 
