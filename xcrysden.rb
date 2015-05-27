2a499efb6f3b9cbb90f74cfafb302156f3988629
Agilio Padua <agilio.padua@gmail.com>
2015-05-27 10:38:33 -0400

    Changes suggested by brew audit

--- xcrysden.rb
+++ xcrysden.rb
@@ -10,7 +10,7 @@ class Xcrysden < Formula
   depends_on "homebrew/dupes/tcl-tk" => "with-x11"
   depends_on "fftw" => "with-fortran"
   depends_on "wget" => :build
-  
+
   # Fix togl -accum false in Tcl and modify Make.sys
   patch do
     url "https://gist.githubusercontent.com/agiliopadua/1f9fd5e8a598018f03b6/raw/9e63c78c0767e9f542f7833bbfb1be339c963035/xcrysden-macosx.patch"
@@ -18,7 +18,7 @@ class Xcrysden < Formula
   end
 
   def install
-    system "cp", "system/Make.macosx-x11", "Make.sys"
+    cp "system/Make.macosx-x11", "Make.sys"
 
     ENV.deparallelize
     system "make", "xcrysden"
@@ -26,7 +26,7 @@ class Xcrysden < Formula
     args = %W[
       prefix=#{prefix}
     ]
-    
+
     system "make", *args, "install"
   end
 
