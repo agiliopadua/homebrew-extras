b60acf76934ded6dfa2111d8043c8e8e66737389
Agilio Padua <agilio.padua@gmail.com>
2015-05-28 16:57:00 -0400

    Added tests to packmol

--- packmol.rb
+++ packmol.rb
@@ -9,10 +9,24 @@ class Packmol < Formula
 
   depends_on :fortran
 
+  resource "examples" do
+    url "http://www.ime.unicamp.br/~martinez/packmol/examples/examples.tar.gz"
+    sha1 "4ba51772c8cf9051e0f0339f5d3c1a28a482d8ff"
+  end
+  
   def install
     system "./configure"
     system "make"
     bin.install("packmol")
-    (share/"packmol").install("solvate.tcl")
+    (share/"packmol").install "solvate.tcl"
+    (share/"packmol/examples").install resource("examples")
+  end
+
+  test do
+    cp share/"packmol/examples/interface.inp", testpath
+    cp share/"packmol/examples/water.xyz", testpath
+    cp share/"packmol/examples/chlor.xyz", testpath
+    cp share/"packmol/examples/t3.xyz", testpath
+    system "packmol < interface.inp"
   end
 end
