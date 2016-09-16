de6dc7497813fe16536f281b07810b05f6d03ecd
Agilio Padua <agilio.padua@gmail.com>
2016-09-16 21:11:33 +0200

    Formula cleanup

--- packmol.rb
+++ packmol.rb
@@ -1,11 +1,9 @@
-require "formula"
-
 class Packmol < Formula
   desc "Packing optimization for molecular dynamics simulations"
   homepage "http://www.ime.unicamp.br/~martinez/packmol/"
   url "http://leandro.iqm.unicamp.br/packmol/versionhistory/packmol-16.228.tar.gz"
   sha256 "ffdb8f3e772926402719dfc0186a36f3753af0917a0c4c12a117b8acbb244cb4"
-  
+
   env :std
 
   depends_on :fortran
@@ -14,7 +12,7 @@ class Packmol < Formula
     url "http://www.ime.unicamp.br/~martinez/packmol/examples/examples.tar.gz"
     sha256 "97ae64bf5833827320a8ab4ac39ce56138889f320c7782a64cd00cdfea1cf422"
   end
-  
+
   def install
     system "./configure"
     system "make"
