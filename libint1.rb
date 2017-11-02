76a21e6777dda50d774d4710ad5b6b37ca4493a8
Agilio Padua <agilio.padua@gmail.com>
2017-11-03 00:53:22 +0100

    Fix

--- libint1.rb
+++ libint1.rb
@@ -2,7 +2,7 @@ class Libint1 < Formula
   desc "Evaluation of molecular integrals over Gaussian functions"
   homepage "https://github.com/evaleev/libint/"
   url "https://github.com/evaleev/libint/archive/v1.zip"
-  version "1.1.6"
+  version "1.2.1"
   sha256 "807cc66c9cc1eb7a8266e807641abbb6c10cd5fee35f2bd3914d7b10cea97489"
 
   depends_on "autoconf" => :build
