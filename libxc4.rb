5c0277c3ec5374088f707bd4f120b9f3ef3a100d
Agilio Padua <agilio.padua@gmail.com>
2018-03-07 16:55:35 +0100

    Added libxc version 4

---
+++ libxc4.rb
@@ -0,0 +1,44 @@
+class Libxc4 < Formula
+  desc "Library of exchange and correlation functionals for codes"
+  homepage "http://octopus-code.org/wiki/Libxc"
+  url "http://www.tddft.org/programs/octopus/down.php?file=libxc/4.0.4/libxc-4.0.4.tar.gz"
+  sha256 "f63f0d67eca45ca0cde4c53bc4b340146f5323dcdbf1f679f13b86623be383d0"
+  revision 1
+
+  depends_on "gcc" # for gfortran
+
+  conflicts_with "libxc"
+  
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--enable-shared",
+                          "FCCPP=gfortran -E -x c",
+                          "CC=#{ENV.cc}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <xc.h>
+      int main()
+      {
+        int major, minor, micro;
+        xc_version(&major, &minor, &micro);
+        printf(\"%d.%d.%d\", major, minor, micro);
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lxc", "-I#{include}", "-o", "ctest"
+    system "./ctest"
+
+    (testpath/"test.f90").write <<~EOS
+      program lxctest
+        use xc_f90_types_m
+        use xc_f90_lib_m
+      end program lxctest
+    EOS
+    system "gfortran", "test.f90", "-L#{lib}", "-lxc", "-I#{include}",
+                       "-o", "ftest"
+    system "./ftest"
+  end
+end
