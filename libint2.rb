223666069c171387ee3dd7b2748871feb49b17fe
Agilio Padua <agilio.padua@gmail.com>
2020-05-15 18:37:06 +0200

    Formula cleanup

--- libint2.rb
+++ libint2.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Libint2 < Formula
   desc "Evaluation of molecular integrals over Gaussian functions"
   homepage "https://github.com/evaleev/libint"
@@ -25,18 +22,5 @@ class Libint2 < Formula
     system "./configure", *args
     system "make"
     system "make", "install"
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test libint`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end
